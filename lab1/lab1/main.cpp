8c75d44cf005335f19cf9dd7430526987745bba1
Tatiana L <Tatiana_Lopez-Guevara@etu.u-bourgogne.fr>
2013-09-29 21:18:45 +0200

    lab1 template

---
+++ lab1/lab1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  lab1
+//
+//  Created by Tatiana on 9/29/13.
+//  Copyright (c) 2013 Tatiana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
